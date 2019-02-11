70b6b089bbc1437de9bad23c583cbd0eee54d583
elaine <elaine.zamudio@gmail.com>
2019-02-10 20:22:11 -0600

    practicing images and links

--- components/src/pages/index.js
+++ components/src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -14,13 +13,18 @@ const IndexPage = () => (
     <p>Distance: 1,800 light-years</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
+ 
     </div>
     <Address>
    <p> Planetary Nebula NGC 5189</p>
-      <Button variant="primary">See More</Button>
+       
+   <Link to="/page-2/">Go to another space image.</Link><br/>
+      <Button variant="primary">something goes here</Button>
     </Address>
- 
+    
   </Layout>
 )
 
 export default IndexPage
+
+
